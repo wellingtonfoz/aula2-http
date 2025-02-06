fe9a449caa970bdc77f753c9137da5e59014ff15
Suporte (Wellington) <Suporte (Wellington)@DESKTOP-KAV3F3P>
2025-02-06 20:04:18 -0300

    dsfsadfds

--- demo/src/main/java/app/controller/CarroController.java
+++ demo/src/main/java/app/controller/CarroController.java
@@ -1,11 +1,15 @@
 package app.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,12 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 import app.entity.Carro;
 import app.service.CarroService;
 
+
 @RestController
 @RequestMapping("/api/carro")
 public class CarroController {
 	
+	
+	
 	@Autowired
 	private CarroService carroService;
+	
+	
 	
 	@PostMapping("/save")
 	public ResponseEntity<String> save(@RequestBody Carro carro){
@@ -47,5 +56,52 @@ public class CarroController {
 
 		}
 	}
+	
+	
+	@GetMapping("/findAll")
+	public ResponseEntity<List<Carro>> findAll(){
+		
+		try {
+			List<Carro> lista = this.carroService.findAll();
+			return new ResponseEntity<>(lista, HttpStatus.OK);
+		} catch (Exception e) {
+			//deu erro... deu problema
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST );
+		}
+		
+	}
+	
+	
+	@PutMapping("/update/{id}")
+	public ResponseEntity<String> update(@RequestBody Carro carro, @PathVariable long id){
+		try {
+			
+			String mensagem = this.carroService.update(carro, id);
+			return new ResponseEntity<>(mensagem, HttpStatus.OK);
+			
+		} catch (Exception e) {
+			//deu erro... deu problema
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST );
+		}
+	}
+	
+	
+	@DeleteMapping("/delete/{id}")
+	public ResponseEntity<String> delete(@PathVariable long id){
+		try {
+			//quando tudo tiver lindo e der certo
+			String mensagem = this.carroService.delete(id);
+			return new ResponseEntity<>(mensagem, HttpStatus.OK );
+			
+		} catch (Exception e) {
+			//deu erro... deu problema
+			return new ResponseEntity<>(null, HttpStatus.BAD_REQUEST );
+
+		}
+	}
+	
+	
+	
+	
 
 }
--- demo/src/main/java/app/service/CarroService.java
+++ demo/src/main/java/app/service/CarroService.java
@@ -1,28 +1,59 @@
 package app.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import app.entity.Carro;
 
 @Service
 public class CarroService {
-	
+
 	public String save(Carro carro) {
-		
+
 		// REFATORAR DEPOIS COM BD
-		
+
 		return "O carro "+carro.getNome()+" foi salvo com sucesso!";
 	}
-	
-	
+
+
 	public Carro findById(long id) {
 		//
 		Carro carro = new Carro(3, "Uno", "Preto");
-		
+
 		if(carro.getId() == id) {
 			return carro;
 		}else
 			return null;
+	}
+
+
+	public List<Carro> findAll(){
+
+		List<Carro> lista = new ArrayList<>();
+
+		lista.add(new Carro(1,"Fiesta", "Preto"));
+		lista.add(new Carro(2,"HRV", "Preto"));
+		lista.add(new Carro(3,"Celta", "Preto"));
+		lista.add(new Carro(4,"Marea", "Preto"));
+
+		return lista;
+	}
+
+	public String update(Carro carro, long id) {
+
+		//comunicação banco
+
+		return "O carro " + carro.getNome() + " foi atualizado com sucesso!";
+	}
+
+
+	public String delete(long id) {
+
+		//comunicação banco
+
+		return "O carro foi deletado com sucesso!";
 	}
 
 }
